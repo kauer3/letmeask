0a9095548e3dfaa7bad2106d282b7b114af6ac07
kauer3 <kaue0607@gmail.com>
2021-06-26 06:27:55 -0300

    Fix copy room class button style

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,23 @@
 import copyImg from "../assets/images/copy.svg";
 
-import "../styles/room-code.scss";
+import '../styles/room-code.scss';
 
-export function RoomCode() {
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
+
+
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #1293dh2387</span>
+      <span>Sala -W#$%codigodasala@#$%</span>
     </button>
   )
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,5 @@
+import {useParams} from "react-router-dom";
+
 import logoImg from "../assets/images/logo.svg";
 
 import {Button} from "../components/Button";
@@ -5,14 +7,19 @@ import {RoomCode} from "../components/RoomCode";
 
 import "../styles/room.scss";
 
+type RoomParams = {
+  id: string;
+}
 
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode />
+          <RoomCode code={params.id} />
         </div>
       </header>
 
